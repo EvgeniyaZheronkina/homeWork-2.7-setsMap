83c225f1fa130eead40a27f6da5dfc402f8b5c9d
zhero <zheronkina86@gmail.com>
2024-07-20 22:54:47 +0300

    new project

---
+++ homeWork2.7SetMaps/src/test/java/pro/sky/homeWork2/_SetMaps/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homeWork2._SetMaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
